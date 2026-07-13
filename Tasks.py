132d2283a0af10f62da3891b32b3629ff42999ae
Leo <leonidvavilov155@gmail.com>
2026-07-13 19:04:13 +0300

    Написал структуру для полей данных

--- Tasks.py
+++ Tasks.py
@@ -1,2 +1,19 @@
 ## Файл с логикой действий проекта
 
+## Описываем структуру полей данных
+class StructDataOfTransactions:
+
+    # Поля структуры (класса)
+
+    IdTransaction = None  # Уникальный ID транзакции
+    Date = None  # Дата транзакции
+    Amount = 0  # Сумма транзакции
+    Description = ""  # Заметка об операции
+
+    #  Инициализатор
+    def __init__(self, date, amount, description):
+        self.Date = date
+        self.Amount = amount
+        self.Description = description
+
+    
