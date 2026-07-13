e46b3990a969acce2936b5c594b7b3e2f7ae56c7
Leo <leonidvavilov155@gmail.com>
2026-07-13 18:43:48 +0300

    Добавил функцию Menu2() в InteractionUC.py

--- Interaction_UC.py
+++ Interaction_UC.py
@@ -1,3 +1,5 @@
+import sys
+
 
 ## Функция вывода 1-го меню
 def Menu1():
@@ -10,19 +12,43 @@ def Menu1():
     print("Изменить данные - 2")
     print("Выйти - 3", end="\n\n")
 
-
-
     myChoice1 = int(input("Сделайте выбор: "))
     print()
 
-
     match myChoice1:
+
+        case 1:
+            Menu2()
+        case 2:
+            ...
+        case 3:
+            sys.exit()
+        case _:
+            print("Попробуйте ещё раз!!!", end="\n\n")
+
+
+
+## Функция вывода 2-го меню со способами вывода информации
+def Menu2():
+    print()
+
+    print()
+
+    print("Посмотреть данные в прямом порядке - 1")
+    print("Посмотреть данные в обратном порядке - 2")
+    print("Посмотреть данные в отсортированном порядке - 3")
+    print("Выйти - 4", end="\n\n")
+
+    myChoice2 = int(input("Сделайте выбор: "))
+    print()
+
+    match myChoice2:
 
         case 1:
             ...
         case 2:
             ...
         case 3:
-            ...
+            sys.exit()
         case _:
             print("Попробуйте ещё раз!!!", end="\n\n")
