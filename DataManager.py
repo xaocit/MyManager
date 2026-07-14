70e26d9a33e1a21cef7c8754700b0402e6846a29
Leo <leonidvavilov155@gmail.com>
2026-07-14 20:26:42 +0300

    Добавил функции записи/перезаписи данных файла в DataManager.py
    Передумал использовать .json - теперь для бд будет файл .jsonc и модуль
    commentjson вместо json - для использования комментариев в .jsonc - файле, хотя
    это понадобится ТОЛЬКО НА ЭТАПЕ РАЗРАБОТКИ, т. к. комментарии уничтожаются после
    перезаписи данных в файле!!!

--- DataManager.py
+++ DataManager.py
@@ -1,2 +1,23 @@
 ## Файл взаимодействия программы с данными в .json - файле
 
+### Импорт модуля, необходимого для работы с .json - файлами
+# import json
+import commentjson
+
+# Функция чтения данных из файла .json при первом открытии
+def readingDataFromFile():
+
+    with open('MyDataBase.jsonc', 'r', encoding='utf-8') as file:
+
+        data = commentjson.load(file)
+
+        return data
+    
+
+# Функция записи данных в файл json. Получает на вход старые данные файла и новые
+def writeDataToFile(data, newData):
+
+    data["transactions"].append(newData)
+
+    with open('MyDataBase.jsonc', 'w', encoding='utf-8') as file:
+        commentjson.dump(data, file, indent=4, ensure_ascii=False)
--- main.py
+++ main.py
@@ -3,8 +3,12 @@
 
 ### Импорты файлов
 import Interaction_UC
+import DataManager
 
 
 if __name__ == "__main__":
+
+    dataBase = DataManager.readingDataFromFile()  # Читаем базу данных на старте и сохраняем в переменную
+
     while True:
         Interaction_UC.Menu1()
