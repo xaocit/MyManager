558a25527908a84c81da33fd8d1d1a3e77a355e7
Leo <leonidvavilov155@gmail.com>
2026-07-14 21:16:08 +0300

    Добавлены функции табличного вывода бд в консоль
    Также убран беск. цикл в main.py - я ошибся тогда ...
    Переименованы некоторые функции в Interaction_UC.py
    А также другие мелочные изменения в коде !!!

--- Interaction_UC.py
+++ Interaction_UC.py
@@ -1,9 +1,16 @@
 import sys
-
+import DataManager
+import Tasks
 
 ## Функция вывода 1-го меню
-def Menu1():
-    
+def Menu1_1():
+
+    # Читаем базу данных и соханяем её перед использованием в программе
+    dataBase = DataManager.readingDataFromFile()  
+
+    ## Формируем список записей данных для удобства
+    listOfDataBase = [Tasks.StructDataOfTransactions(i["id"], i["date"], i["amount"], i["type"], i["description"]) for i in dataBase["transactions"]]
+
     print("Здравствуйте! Вы в менеджере ваших расходов и доходов. Что вы хотите сделать?", end="\n\n")
 
     print()
@@ -18,18 +25,19 @@ def Menu1():
     match myChoice1:
 
         case 1:
-            Menu2()
+            Menu1_2(listOfDataBase)
         case 2:
             ...
         case 3:
             sys.exit()
         case _:
             print("Попробуйте ещё раз!!!", end="\n\n")
+            Menu1_1()
 
 
 
 ## Функция вывода 2-го меню со способами вывода информации
-def Menu2():
+def Menu1_2(listOfDataBase):
     print()
 
     print()
@@ -45,10 +53,28 @@ def Menu2():
     match myChoice2:
 
         case 1:
-            ...
+            pprintDataForward(listOfDataBase, 1)
+            Menu1_2(listOfDataBase)
         case 2:
-            ...
+            pprintDataForward(listOfDataBase, -1)
+            Menu1_2(listOfDataBase)
         case 3:
+            ...
+        case 4:
             sys.exit()
         case _:
             print("Попробуйте ещё раз!!!", end="\n\n")
+            Menu1_2(listOfDataBase)
+
+
+## Функция красивого вывода данных в прямом или обратном порядке
+def pprintDataForward(listOfDataBase, mode):
+
+    # Красивый вывод в виде таблицы
+    print("-" * 80)
+    print(f"{'ID':<5} {'Дата':<12} {'Сумма':<10} {'Тип':<15} {'Описание'}")
+    print("-" * 80)
+    for item in listOfDataBase[::mode]:
+        print(f"{item.IdTransaction:<5} {item.Date:<12} {item.Amount:<10.2f} {item.TypeOfOperation:<15} {item.Description}")
+    print("-" * 80)
+
