7df91f8560513f032e14db64a27e8de6d9ae1019
Leo_laptop <leonidvavilov155@gmail.com>
2026-08-22 12:49:30 +0300

    Красиво оформил весь код - поработал над комментариями

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Главный файл (Точка входа)
 
+### Импортируем классы и данные, необходимые для запуска ###
+
 from infrastructure.data_manager import JsonRepository, JsonFormatter
 
 from services.data_service import TransactionService, TransactionSorter
@@ -9,20 +11,30 @@ from ui.ui_service import ConsoleUI
 
 from config import DATA_FILE_PATH
 
+### Конец импорта ###
 
 def main():
+    """Главная Функция, где собираем зависимости и запускаем ui - интерфейс"""
+
+    # Класс форматирования списка из вида python в вид, характерный для .json - файла, и наоборот
     formatter = JsonFormatter()
 
+    # Собираем в репозитории наш путь к файлу бд и логику форматирования (перевода) данных в formatter
     repository = JsonRepository(DATA_FILE_PATH, formatter)
 
+    # Инициализируем объект сортера
     sorter = TransactionSorter()
 
+    # Собираем в service наш кастомный json-репозиторий и свой сортер для сортировки данных
     service = TransactionService(repository, sorter)
 
+    # Инициализируем валидатор
     validator = ValidatorOfInputData()
 
+    # Собираем все предыдущие объекты (service, validator) в классе по работе с консольным UI
     ui = ConsoleUI(service, validator)
 
+    # Запускаем приложение включением ui
     ui.run()
 
 
