e6e28d73a469da60ebd2bbdda6d917a1b929e73e
Leo <leonidvavilov155@gmail.com>
2026-08-25 15:31:47 +0300

    Добавлена валидация по количеству вводимых значений при опции add() или change()

--- services/data_input_validator.py
+++ services/data_input_validator.py
@@ -3,6 +3,21 @@
 from typing import Union
 import re
 
+class ValidateCountOfValues():
+    """Класс, реализующий методы по валидации количества переданных значений"""
+
+    def is_correct_count_of_values(self, string_of_data: str, count_fields: int) -> Union[str, bool]:
+
+        # Количество пробелов между полями
+        count_of_spaces = count_fields - 1
+
+        # Пытаемся распаковать строку
+        if len(string_of_data.split(None, count_of_spaces)) != count_fields:
+            return f"Ошибка! Вы должны передать {count_fields} значений через пробел !!!"
+
+        # Если всё хорошо
+        return True
+
 class ValidateDate():
     """Класс, реализующий методы по валидации даты"""
 
@@ -14,8 +29,7 @@ class ValidateDate():
         if bool(re.match(self._date_format, new_date)):
             return True
 
-        return "Ошибка! Неверный формат даты"
-
+        return "Ошибка! Неверный формат даты !!!"
 
 class ValidateAmount():
     """Класс, реализующий методы по валидации Суммы"""
@@ -32,7 +46,6 @@ class ValidateAmount():
         
         return "Ошибка! Сумма должна быть числом !!!"
 
-
 class ValidateTypeOfOperation():
     """Класс, реализующий методы по валидации типа операции"""
 
@@ -41,15 +54,27 @@ class ValidateTypeOfOperation():
         if new_operation in ("expense", "income"):
             return True
 
-        return "Ошибка! Тип операции должен быть 'expense' или 'income'"
+        return "Ошибка! Тип операции должен быть 'expense' или 'income' !!!"
 
-
-class ValidatorOfInputData(ValidateDate, ValidateAmount, ValidateTypeOfOperation):
+class ValidatorOfInputData(ValidateCountOfValues, ValidateDate, ValidateAmount, ValidateTypeOfOperation):
     """Класс, реализующий пользовательскую валидацию для проверки вводимых данных, 
     он же - класс-сборщик предыдущих валидаторов"""
 
-    def is_correct_input(self, input_date: str, input_amount: str, input_operation: str) -> Union[list, bool]:
+    def is_correct_input(self, my_new_data_in_bd: str, correct_count_of_fields: int) -> Union[str, bool]:
+        """Метод, реализующий валидацию введённой строки с данными"""
 
+        # Валидируем количество переданных полей
+        result_of_validate_count_of_fields = self.is_correct_count_of_values(
+            my_new_data_in_bd,
+            correct_count_of_fields
+            )
+
+        # В самом начале возвращаем ошибку валидации количества полей, если она есть
+        if isinstance(result_of_validate_count_of_fields, str):
+            return result_of_validate_count_of_fields
+        
+        input_date, input_amount, input_operation, _ = my_new_data_in_bd.split(None, correct_count_of_fields)
+        
         result_of_validate_date = self.is_correct_date(input_date)
         result_of_validate_amount = self.is_correct_amount(input_amount)
         result_of_validate_type = self.is_correct_type_of_operation(input_operation)
@@ -65,7 +90,7 @@ class ValidatorOfInputData(ValidateDate, ValidateAmount, ValidateTypeOfOperation
         list_of_results = (result_of_validate_date, result_of_validate_amount, \
                            result_of_validate_type)
 
-        # Формируем список ошибок для вывода
-        messages_errors_of_validate = [i for i in list_of_results if isinstance(i, str)]
-
-        return messages_errors_of_validate
+        # Возвращаем 1-ю попавшуюся ошибку
+        for i in list_of_results:
+            if isinstance(i, str):
+                return i
